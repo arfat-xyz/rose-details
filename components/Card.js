2450133a25dd44d0e72258c461de3a32b412647f
arfat-beep <arfatrahman08@gmail.com>
2023-01-20 12:52:19 +0600

    first commit

---
+++ components/Card.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Card = ({ image, heading, desc, btnLink }) => {
+  return (
+    <>
+      <div className=" hover:scale-105 transition duration-500 max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <Link href={btnLink}>
+          <Image
+            className="rounded-t-lg w-full h-full"
+            src={image}
+            alt=""
+            width="0"
+            height="100"
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </Link>
+        <div className="p-5">
+          <Link href={btnLink}>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {heading}
+            </h5>
+          </Link>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {desc}
+          </p>
+          <Link
+            href={btnLink}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-lime-400 rounded-lg focus:ring-4 focus:outline-none transition duration-500 focus:ring-blue-300 dark:bg-green-500 dark:hover:bg-lime-700 dark:focus:ring-blue-800"
+          >
+            {" "}
+            More details
+            <svg
+              aria-hidden="true"
+              className="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ pages/rose.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+
+const rose = () => {
+  return (
+    <>
+      <div className="container mx-auto dark:text-white text-xl">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
+          nostrum illum iure, illo dolores tempore eius fugit reiciendis dicta
+          voluptate! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Exercitationem rerum quaerat corporis, odio maiores voluptatum animi
+          tempore veniam ipsum recusandae atque esse mollitia quo nemo quis?
+          Tempore deserunt, optio sed reiciendis blanditiis atque laudantium
+          dolore repellendus, a obcaecati excepturi dolorum nemo amet ad
+          pariatur? Dolorum a, inventore ad maxime necessitatibus laboriosam
+          molestias exercitationem similique id maiores, temporibus error ipsa
+          sit.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, nulla.
+        </p>
+        <br />
+        <Image
+          src={"/rose-cover.jpg"}
+          className="rounded-t-lg w-full h-40"
+          alt=""
+          width="0"
+          height="100"
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint eius,
+          eos vel neque deleniti dolor ipsum debitis, placeat a adipisci nihil
+          quisquam veniam consequatur, aspernatur recusandae laboriosam
+          provident voluptatibus mollitia.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default rose;
