b45411eb163c4c3334e2e815c14fb53e325e7260
arfat-beep <arfatrahman08@gmail.com>
2023-01-20 15:34:34 +0600

    final

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <>
-      <footer className="text-center sticky top-[100vh] border-t-2 border-t-slate-600 text-white">
+      <footer className="text-center sticky top-[100vh] p-3 border-t-2 border-t-slate-600 text-white">
         <div className="container mx-auto px-6 pt-6">
           <div className="flex justify-center mb-6">
-            <a
-              href="#!"
+            <Link
+              href="https://www.facebook.com/saifullahchy.emon.9"
               type="button"
               className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1"
             >
@@ -26,10 +27,10 @@ const Footer = () => {
                   d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
                 ></path>
               </svg>
-            </a>
+            </Link>
 
-            <a
-              href="#!"
+            <Link
+              href="https://www.facebook.com/saifullahchy.emon.9"
               type="button"
               className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1"
             >
@@ -48,7 +49,7 @@ const Footer = () => {
                   d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
                 ></path>
               </svg>
-            </a>
+            </Link>
           </div>
         </div>
 
@@ -56,10 +57,14 @@ const Footer = () => {
           className="text-center p-4"
           //   style="background-color: rgba(0, 0, 0, 0.2);"
         >
-          © 2021 Copyright:
-          <a className="text-white" href="https://tailwind-elements.com/">
-            Tailwind Elements
-          </a>
+          © 2023 Copyright :
+          <Link
+            className="text-white"
+            href="https://www.facebook.com/saifullahchy.emon.9"
+          >
+            {" "}
+            Saifullah Chy Emon
+          </Link>
         </div>
       </footer>
       ;
